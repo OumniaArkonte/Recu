1525028a0c7ae404eb6b7d6f47b9fffaf07ac7af
ORIGINAL SHOP <arkonteoumnia@gmail.com>
2023-03-25 14:16:09 +0000

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,24 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import {HttpClientModule} from "@angular/common/http";
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { RecuCreateComponent } from './view/recus/recu-create/recu-create.component';
+import { RecuListComponent } from './view/recus/recu-list/recu-list.component';
+import {FormsModule} from "@angular/forms";
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RecuCreateComponent,
+    RecuListComponent,
+
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
